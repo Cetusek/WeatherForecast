55ae5624957b3478af7e979671d1611d30ed9ce7
Marek <mjozwik72@gmail.com>
2021-07-09 19:40:27 +0200

    .

--- app/src/main/java/pl/marek/weatherforecast/blitzortung/URLs.java
+++ app/src/main/java/pl/marek/weatherforecast/blitzortung/URLs.java
@@ -3,7 +3,7 @@ package pl.marek.weatherforecast.blitzortung;
 
 public class URLs {
 
-    public static final String URL_for_Poland = "http://images.blitzortung.org/Images/image_b_pl.png";
+    public static final String URL_for_Poland = "https://www.blitzortung.org/Images/image_b_pl.png";
 
 
 }
